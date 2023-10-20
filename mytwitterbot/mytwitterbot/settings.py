b2d763ccb6febb4643f5919011773f1457cf5f4f
Mujahed Al-Tahleh <mujahed@ieee.org>
2023-10-20 01:29:42 +0000

    add db info

--- mytwitterbot/mytwitterbot/settings.py
+++ mytwitterbot/mytwitterbot/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,8 +100,12 @@ WSGI_APPLICATION = 'mytwitterbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('tweet'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST','localhost'),
+        'PORT': os.getenv('DB_PORT','3306'),
     }
 }
 
