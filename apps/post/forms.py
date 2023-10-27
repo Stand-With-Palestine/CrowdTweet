32d755fbaf7551a84a6f49883717f11096b9c1ea
abedzalloum <abdalhafeez@sitech.me>
2023-10-27 23:09:35 +0300

    translate the content and media file

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -1,6 +1,7 @@
 from crispy_forms.helper import FormHelper
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext as _
 
 
 class PostToTwitterForm(forms.Form):
@@ -8,22 +9,26 @@ class PostToTwitterForm(forms.Form):
         widget=forms.Textarea(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Enter some content'
+                'placeholder': 'Enter some content',
             }
-        )
+        ),
+        label=_('Content')
     )
     media_file = forms.FileField(
         widget=forms.FileInput(
             attrs={
-                'class': 'form-control-file'
+                'class': 'form-control-file',
+                'label': _('Media File')
             }
-        )
+        ),
+        label=_('Media File')
     )
 
     def __init__(self, *args, **kwargs):
         super(PostToTwitterForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.fields['media_file'].required = False
+        # self.fields['media_file'].widget.
 
 
 class LoginForm(AuthenticationForm):
