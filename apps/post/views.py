a2bf8c1e579c16f05c88752f9c188cfbc143cb4a
abedzalloum <abdalhafeez@sitech.me>
2023-10-28 16:19:29 +0300

    use a django model to save the tokens per user instead of using files

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,3 +1,5 @@
+import logging
+
 import tweepy
 from django.conf import settings
 from django.contrib.auth import logout
@@ -5,20 +7,26 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
 )
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView
+)
 from django.http import (
     HttpResponse,
     HttpResponseRedirect
 )
 from django.shortcuts import redirect
-from django.urls import reverse_lazy, reverse
+from django.urls import (
+    reverse_lazy,
+    reverse
+)
 from django.views.generic import (
     FormView,
     TemplateView
 )
-
 from .forms import (
-    PostToTwitterForm, LoginForm
+    PostToTwitterForm,
+    LoginForm
 )
 from .twitter_auth import get_client
 from .utils import (
@@ -26,6 +34,7 @@ from .utils import (
     load_tokens_from_file,
     save_tokens_to_file
 )
+from .models import TwitterUsers
 
 
 class CustomLoginView(LoginView):
@@ -70,29 +79,28 @@ class PostToTwitter(LoginRequiredMixin, UserPassesTestMixin, FormView):
     def post(self, request, *args, **kwargs):
         content = request.POST.get('content', '')
         uploaded_file = request.FILES.get('media_file')
-        file_path = ''
-        if uploaded_file:
-            # Save the uploaded file to the specified directory.
-            file_path = handle_uploaded_file(uploaded_file)
-            print("File Path:", file_path)
-
-        results = []
         if content:
-            print('Content:', content)
-            values = load_tokens_from_file()
-            for value in values:
-                access_token, access_secret = values[value]
-
-                client = get_client(access_token, access_secret)
-
+            for user in TwitterUsers.objects.all():
+                access_token = user.tokens['access_token']
+                access_secret = user.tokens['access_secret']
+                client = get_client(
+                    access_token,
+                    access_secret
+                )
                 try:
                     # Use API V1 to get media ID
-                    settings.AUTH.set_access_token(access_token, access_secret)
+                    settings.AUTH.set_access_token(
+                        access_token,
+                        access_secret
+                    )
                     client_v1 = tweepy.API(settings.AUTH)
                     if uploaded_file:
-                        media = client_v1.media_upload(file_path)
+                        media = client_v1.media_upload(
+                            handle_uploaded_file(
+                                uploaded_file
+                            )
+                        )
                         media_id = media.media_id
-
                         client.create_tweet(
                             text=content,
                             media_ids=[media_id]
@@ -101,15 +109,12 @@ class PostToTwitter(LoginRequiredMixin, UserPassesTestMixin, FormView):
                         client.create_tweet(
                             text=content,
                         )
-                    results.append(f"Tweet posted successfully for {value}")
 
                 except tweepy.errors.TweepyException as e:
-                    print(f"Twitter API Error for {value}: {e}")
-                    results.append(f"Error posting the tweet for {value}")
+                    logging.error(f"Twitter API Error for {user}: {e}")
 
                 except Exception as e:
-                    print(f"Error posting the tweet for {value}: {e}")
-                    results.append(f"Error posting the tweet for {value}")
+                    logging.error(f"Error posting the tweet for {user}: {e}")
 
         return HttpResponseRedirect(reverse('post:post_to_twitter'))
 
@@ -139,8 +144,12 @@ def twitter_callback_sso(request):
     auth.request_token = request_token
     try:
         access_secret = auth.get_access_token(verifier)
-        # Save the user's access_token and access_secret to a file or database
-        save_tokens_to_file(access_secret)
+        tokens_dict = {
+            'access_token': access_secret[0],
+            'access_secret': access_secret[1]
+        }
+        users = TwitterUsers(tokens=tokens_dict)
+        users.save()
         return redirect('post:welcome_page')
     except tweepy.errors.TweepyException as e:
         return HttpResponse("Error: %s" % e)
