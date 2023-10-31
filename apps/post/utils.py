ed93ef4936bb33f4bf0738031c9667ee864fb529
abedzalloum <abdalhafeez@sitech.me>
2023-10-31 21:04:32 +0300

    update the security issue in a handle with upload files

--- apps/post/utils.py
+++ apps/post/utils.py
@@ -7,13 +7,10 @@ def handle_uploaded_file(uploaded_file):
     # Define the directory where you want to save the file, relative to your media root.
     upload_directory = 'uploads/'  # This is a subdirectory within the media root.
 
-    # Protected Man check here !!
-
-    # directory = os.path.basename(uploaded_file)
-    # upload_file_name = os.path.splitext(directory)[0]
+    file = os.path.splitext(os.path.basename(uploaded_file))[0]
 
     # Create the full path for the uploaded file.
-    file_path = os.path.join(settings.MEDIA_ROOT, upload_directory, uploaded_file.name)
+    file_path = os.path.join(settings.MEDIA_ROOT, upload_directory, file)
 
     # Open the file and save it to the desired location.
     with open(file_path, 'wb') as destination:
