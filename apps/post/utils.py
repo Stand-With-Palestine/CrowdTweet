0efce9a8bd5bcb43ca976df15c137594a965b6bb
abedzalloum <abdalhafeez@sitech.me>
2023-10-31 18:17:34 +0300

    fix the security issue and the duplicate users

--- apps/post/utils.py
+++ apps/post/utils.py
@@ -7,6 +7,11 @@ def handle_uploaded_file(uploaded_file):
     # Define the directory where you want to save the file, relative to your media root.
     upload_directory = 'uploads/'  # This is a subdirectory within the media root.
 
+    # Protected Man check here !!
+
+    # directory = os.path.basename(uploaded_file)
+    # upload_file_name = os.path.splitext(directory)[0]
+
     # Create the full path for the uploaded file.
     file_path = os.path.join(settings.MEDIA_ROOT, upload_directory, uploaded_file.name)
 
